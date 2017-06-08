a82d475490f580da591477686d4247d8d09098fd
littleflute <13699175164@sina.cn>
2017-06-08 15:50:47 -0500

    Add files via upload

---
+++ Master of Puppets/m.js
@@ -0,0 +1,10 @@
+var f = [];				
+f[0]	= "v0.0.8";			
+f[1]	= "01 Battery.mp3";		
+f[2]	= "02 Master of Puppets.mp3";		
+f[3]	= "03 The Thing That Should Not Be.mp3";		
+f[4]	= "04 Welcome Home (Sanitarium).mp3";		
+f[5]	= "05 Disposable Heroes.mp3";		
+f[6]	= "06 Leper Messiah.mp3";		
+f[7]	= "07 Orion [Instrumental].mp3";		
+f[8]	= "08 Damage, Inc.mp3";		
